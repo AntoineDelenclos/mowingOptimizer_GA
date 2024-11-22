c3b18fc2c6f2c28e074ddeebf6b0a603e1433012
Antoine Delenclos <antoine.delenclos37@gmail.com>
2024-11-22 10:31:02 +0100

    Changes by quentin

--- src/main.py
+++ src/main.py
@@ -1,7 +1,30 @@
+import random
+from deap import creator, base, tools, algorithms
+
+# creator.create("FitnessMin", base.Fitness, weights=(-1.0,))  # Minimisation de la fitness
+# creator.create("Individual", list, fitness=creator.FitnessMin)
+#
+# toolbox = base.Toolbox()
+# toolbox.register("attr_move", lambda: random.choice([(0,1), (1,0), (0,-1), (-1,0)]))
+# toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_move, n=100)
+# toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+#
+# toolbox.register("mate", tools.cxTwoPoint)
+# toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.2)
+# toolbox.register("select", tools.selTournament, tournsize=3)
+# toolbox.register("evaluate", fitness_function)
+
 
 
 def main():
-    return 0
+    # population initiale
+    #population = toolbox.population(n=100)
+
+    # Paramètres de l'algorithme évolutionnaire
+    ngen = 50  # Nombre de générations
+    cxpb = 0.7  # Probabilité de croisement
+    mutpb = 0.2  # Probabilité de mutation
+
 
 if __name__ == '__main__':
     main()
