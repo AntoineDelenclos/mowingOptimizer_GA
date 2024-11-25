a1224f59e864d35024aa131532f9af65ad70af6c
quentin <quentin.tollot@orange.fr>
2024-11-25 08:05:30 +0100

    update of the project

--- src/main.py
+++ src/main.py
@@ -1,30 +1,8 @@
-import random
-from deap import creator, base, tools, algorithms
-
-# creator.create("FitnessMin", base.Fitness, weights=(-1.0,))  # Minimisation de la fitness
-# creator.create("Individual", list, fitness=creator.FitnessMin)
-#
-# toolbox = base.Toolbox()
-# toolbox.register("attr_move", lambda: random.choice([(0,1), (1,0), (0,-1), (-1,0)]))
-# toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_move, n=100)
-# toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-#
-# toolbox.register("mate", tools.cxTwoPoint)
-# toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.2)
-# toolbox.register("select", tools.selTournament, tournsize=3)
-# toolbox.register("evaluate", fitness_function)
-
-
+from src.mower import *
+from src.fitness import *
 
 def main():
-    # population initiale
-    #population = toolbox.population(n=100)
-
-    # Paramètres de l'algorithme évolutionnaire
-    ngen = 50  # Nombre de générations
-    cxpb = 0.7  # Probabilité de croisement
-    mutpb = 0.2  # Probabilité de mutation
-
+    print("vide")
 
 if __name__ == '__main__':
     main()
